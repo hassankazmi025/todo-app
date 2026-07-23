2e83e2256cd0acb7eb6535b874f8ceec73db3d0d
Hassan Kazmi <hassankazmi025@gmail.com>
2026-07-23 06:45:02 +0500

    my todo

---
+++ assignment.js
@@ -0,0 +1,14 @@
+console.log(23 + 97 + 34 + 78 + 10 + 20);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+a = 20;
+console.log(a);
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage)
